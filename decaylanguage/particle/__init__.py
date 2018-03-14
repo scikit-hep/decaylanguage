93eec2bd3b7b8df53ced236ebc3c52566089ed1a
Henry Fredrick Schreiner <henry.fredrick.schreiner@cern.ch>
2018-03-14 12:45:55 +0100

    Adding tests for particle

--- decaylanguage/particle/__init__.py
+++ decaylanguage/particle/__init__.py
@@ -1,2 +1,2 @@
 from __future__ import absolute_import
-from .particle import Particle, SpinType, Par_mapping, programatic_name
+from .particle import Particle, SpinType, Par_mapping, Par, Charge, Inv, Status, programatic_name
--- tests/test_particle.py
+++ tests/test_particle.py
@@ -1,4 +1,21 @@
-from decaylanguage.particle import Particle
+from decaylanguage.particle import Particle, Par
 
 def test_pdg():
     assert Particle.from_pdg(211).val == 211
+
+def test_str():
+    pi = Particle.from_pdg(211)
+    assert str(pi) == 'pi+'
+
+def test_rep():
+    pi = Particle.from_pdg(211)
+    assert 'val=211' in repr(pi)
+    assert "name='pi'" in repr(pi)
+    assert 'mass=0.13957' in repr(pi)
+    assert 'charge=<Par.p: 1>' in repr(pi)
+
+def test_prop():
+    pi = Particle.from_pdg(211)
+    assert pi.name == 'pi'
+    assert pi.val == 211
+    assert pi.charge == Par.p
