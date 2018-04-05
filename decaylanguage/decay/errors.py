d63081a08dec33cb9d5727f8871d4ed1c0a83fdc
Henry Fredrick Schreiner <henry.fredrick.schreiner@cern.ch>
2018-04-05 16:44:33 +0200

    Fix flake8 error

--- decaylanguage/decay/errors.py
+++ decaylanguage/decay/errors.py
@@ -3,7 +3,6 @@ from __future__ import division
 from __future__ import print_function
 
 
-
 class LineFailure(RuntimeError):
-    def __init__(self, line, message, *args,**kwargs):
+    def __init__(self, line, message, *args, **kwargs):
         super(LineFailure, self).__init__("{0}: {1}".format(line, message), *args, **kwargs)
