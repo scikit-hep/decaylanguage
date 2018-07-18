6fb67a660843c226661beaf694c25a6e02c25246
eduardo-rodrigues <eduardo.rodrigues@cern.ch>
2018-07-18 17:50:40 +0200

    Tidy-up suggestions, pleasing AppVeyor

--- decaylanguage/utils/__init__.py
+++ decaylanguage/utils/__init__.py
@@ -1,6 +1,8 @@
 from __future__ import absolute_import
 
 from .errors import LineFailure
-from .utilities import iter_flatten, split, filter_lines
+from .utilities import iter_flatten
+from .utilities import split
+from .utilities import filter_lines
 
 __all__ = (LineFailure, iter_flatten, split, filter_lines)
