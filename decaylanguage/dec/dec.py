4bdff4dd9ed96508542719396ef6e1862fbef1df
eduardo-rodrigues <eduardo.rodrigues@cern.ch>
2019-04-02 14:45:05 +0200

    Add new DecFileNotParsed class, with test suite

--- decaylanguage/dec/dec.py
+++ decaylanguage/dec/dec.py
@@ -1,11 +1,224 @@
 from __future__ import absolute_import, division, print_function
 
+import os
+import warnings
 from lark import Lark, Transformer, Tree
 
 from ..data import open_text
 from .. import data
 from ..decay.decay import Decay
 from .enums import PhotosEnum
+
+
+class DecFileNotParsed(RuntimeError):
+    pass
+
+
+class DecayNotFound(RuntimeError):
+    pass
+
+
+class DecFileParser(object):
+    """
+    The class to parse a .dec decay file.
+
+    Example
+    -------
+    >>> p = DecFileParser('my-decay-file.dec')
+    >>> p.parse()
+    """
+    def __init__(self, decay_file):
+        """
+        Constructor.
+
+        Parameters
+        ----------
+        decay_file: str
+            Input .dec decay file.
+        """
+        # Check input file
+        if not os.path.exists(decay_file):
+            raise FileNotFoundError("'{0}'!".format(decay_file))
+
+        self._grammar = None       # Loaded Lark grammar definition file
+        self._grammar_info = None  # Name of Lark grammar definition file
+
+        self._decay_file = decay_file  # Name of the input decay file
+        self._parsed_dec_file = None   # Parsed decay file
+
+        self._parsed_decays = None  # Particle decays found in the decay file
+
+    def parse(self):
+        # Has a fine been parsed already?
+        if self._parsed_decays is not None:
+            warnings.warn("Input file being re-parsed ...")
+
+        decay_file = open(self._decay_file).read()
+        parser = Lark(self.grammar, parser='lalr', lexer='standard')
+
+        self._parsed_dec_file = parser.parse(decay_file)
+
+        self._find_parsed_decays()
+
+    @property
+    def grammar(self):
+        """
+        This accesses the internal Lark grammar definition file,
+        loading it from the default location if needed.
+        """
+        if not self.grammar_loaded:
+            self.load_grammar()
+
+        return self._grammar
+
+    def load_grammar(self, filename=None, parser='lalr', lexer='standard'):
+        """
+        Load a Lark grammar definition file.
+        """
+        if filename is None:
+            filename = 'decfile.lark'
+            with data.open_text(data, filename) as f:
+                self._grammar = f.read()
+        else:
+            self._grammar = open(filename).read()
+
+        self._grammar_info = dict(lark_file=filename, parser=parser, lexer=lexer)
+
+    @property
+    def grammar_loaded(self):
+        """Check to see if the Lark grammar definition file is loaded."""
+        return self._grammar is not None
+
+    def _find_parsed_decays(self):
+        """Find all Tree instances of Tree.data='decay'."""
+        if self._parsed_dec_file is not None:
+            self._parsed_decays = tuple(self._parsed_dec_file.find_data('decay'))
+
+        # Check for duplicates - should be considered a bug in the .dec file!
+        self._check_parsed_decays()
+
+        return self._parsed_decays
+
+    def _check_parsing(self):
+        if self._parsed_dec_file is None:
+            raise DecFileNotParsed("Hint: call 'parse()'!")
+
+    def _check_parsed_decays(self):
+        lmn = self.list_decay_mother_names()
+        if self.number_of_decays != len(set(lmn)):
+            warnings.warn("Input .dec file redefines decays for particle(s) {0}!".format(set([n for n in lmn if lmn.count(n)>1])))
+
+    @property
+    def number_of_decays(self):
+        """Returns the number of particle decays defined in the parsed .dec file."""
+        self._check_parsing()
+
+        return len(self._parsed_decays)
+
+    def list_decay_mother_names(self):
+        self._check_parsing()
+
+        return [get_decay_mother_name(d) for d in self._parsed_decays]
+
+    def _find_decay_modes(self, mother):
+        self._check_parsing()
+
+        for decay_Tree in self._parsed_decays:
+            if get_decay_mother_name(decay_Tree) == mother:
+                return tuple(decay_Tree.find_data('decayline'))
+
+        raise DecayNotFound("Decays of particle '%s' not found in .dec file!" % mother)
+
+    def list_decay_modes(self, mother):
+        return [get_final_state_particle_names(mode)
+                for mode in self._find_decay_modes(mother)]
+
+    def get_decay_mode_details(self, decay_mode):
+        """
+        Parse a decay mode (Tree instance)
+        and return the relevant bits of information in it.
+        """
+
+        bf = get_branching_fraction(decay_mode)
+        fsp_names = get_final_state_particle_names(decay_mode)
+        model = get_model_name(decay_mode)
+        model_params = get_model_parameters(decay_mode)
+
+        return (bf, fsp_names, model, model_params)
+
+    def print_decay_modes(self, mother):
+        """Pretty print of the decay modes of a given particle."""
+        dms = self._find_decay_modes(mother)
+
+        for dm in dms:
+            dm_details = self.get_decay_mode_details(dm)
+            print('%12g : %50s %15s %s' % (dm_details[0], '  '.\
+                join(p for p in dm_details[1]), dm_details[2], dm_details[3]))
+
+    def __repr__(self):
+        if self._parsed_dec_file is not None:
+            return "<{self.__class__.__name__}: decfile='{decfile}', n_decays={n_decays}>".format(
+                self=self, decfile=self._decay_file, n_decays=self.number_of_decays)
+        else:
+            return "<{self.__class__.__name__}: decfile='{decfile}'>"\
+                    .format(self=self, decfile=self._decay_file)
+
+    def __str__(self):
+        return repr(self)
+
+
+def get_decay_mother_name(decay_tree):
+    if not isinstance(decay_tree, Tree) or decay_tree.data != 'decay':
+        raise RuntimeError("Input not an instance of a 'decay' Tree!")
+
+    # For a 'decay' Tree, tree.children[0] is the mother particle Tree
+    # and tree.children[0].children[0].value is the mother particle name
+    return decay_tree.children[0].children[0].value
+
+
+def get_branching_fraction(decay_mode):
+    if not isinstance(decay_mode, Tree) or decay_mode.data != 'decayline':
+        raise RuntimeError("Check your input, not an instance of a 'decayline' Tree!")
+
+    # For a 'decayline' Tree, Tree.children[0] is the branching fraction Tree
+    # and tree.children[0].children[0].value is the BF stored as a str
+    try:  # the branching fraction value as a float
+        return float(decay_mode.children[0].children[0].value)
+    except RuntimeError:
+        raise RuntimeError("'decayline' Tree does not seem to have the usual structure. Check it.")
+
+
+def get_final_state_particles(decay_mode):
+    if not isinstance(decay_mode, Tree) or decay_mode.data != 'decayline':
+        raise RuntimeError("Input not an instance of a 'decayline' Tree!")
+
+    # list of Trees of final-state particles
+    return list(decay_mode.find_data('particle'))
+
+
+def get_final_state_particle_names(decay_mode):
+    if not isinstance(decay_mode, Tree) or decay_mode.data != 'decayline':
+        raise RuntimeError("Input not an instance of a 'decayline' Tree!")
+
+    fsps = get_final_state_particles(decay_mode)
+    # list of final-state particle names
+    return [fsp.children[0].value for fsp in fsps]
+
+
+def get_model_name(decay_mode):
+    if not isinstance(decay_mode, Tree) or decay_mode.data != 'decayline':
+        raise RuntimeError("Input not an instance of a 'decayline' Tree!")
+
+    lm = list(decay_mode.find_data('model'))
+    return lm[0].children[0].value
+
+
+def get_model_parameters(decay_mode):
+    if not isinstance(decay_mode, Tree) or decay_mode.data != 'decayline':
+        raise RuntimeError("Input not an instance of a 'decayline' Tree!")
+
+    lmo = list(decay_mode.find_data('model_options'))
+    return lmo if len(lmo) == 1 else ''
 
 
 class TreeToDec(Transformer):
@@ -24,8 +237,6 @@ class TreeToDec(Transformer):
         return str(item)
     def photos(self, items):
         return PhotosEnum.yes
-
-
 
 
 def define(transformed):
