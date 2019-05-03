cbb5a87847e52cc66b3254e6ffdd453b69470687
eduardo-rodrigues <eduardo.rodrigues@cern.ch>
2019-05-03 22:47:19 +0200

    Make available the list of known decay models

--- decaylanguage/dec/__init__.py
+++ decaylanguage/dec/__init__.py
@@ -1,3 +1,5 @@
 from __future__ import absolute_import
 
 from .dec import DecFileParser
+
+from .enums import known_decay_models
--- decaylanguage/dec/enums.py
+++ decaylanguage/dec/enums.py
@@ -1,5 +1,5 @@
 """
-Collection of enums to help characterising .dec decay files.
+Collection of enums and info to help characterising .dec decay files.
 """
 
 # Backport needed if Python 2 is used
@@ -9,3 +9,49 @@ from enum import IntEnum
 class PhotosEnum(IntEnum):
     no = 0
     yes = 1
+
+
+# This list should match the list specified in the decay file parser file
+# 'decaylanguage/data/decfile.lark'!
+known_decay_models = (
+    "BaryonPCR",
+    "BTO3PI_CP",
+    "BTOSLLALI",
+    "BTOSLLBALL",
+    "BTOXSGAMMA",
+    "BTOXSLL",
+    "CB3PI-MPP",
+    "CB3PI-P00",
+    "D_DALITZ",
+    "ETA_DALITZ",
+    "GOITY_ROBERTS",
+    "HELAMP",
+    "HQET",
+    "ISGW2",
+    "LbAmpGen",
+    "OMEGA_DALITZ",
+    "PARTWAVE",
+    "PHSP",
+    "PI0_DALITZ",
+    "PYTHIA",
+    "SLN",
+    "SSD_CP",
+    "STS",
+    "SVP_HELAMP",
+    "SVS",
+    "SVV_HELAMP",
+    "TAUHADNU",
+    "TAULNUNU",
+    "TAUSCALARNU",
+    "TAUVECTORNU",
+    "TSS",
+    "TVS_PWAVE",
+    "VLL",
+    "VSP_PWAVE",
+    "VSS",
+    "VSS_BMIX",
+    "VUB",
+    "VVP",
+    "VVPIPI",
+    "VVS_PWAVE",
+    )
