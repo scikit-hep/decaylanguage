fb8c1cb7c0fbd56e1667b645c0a894d088d9beb7
eduardo-rodrigues <eduardo.rodrigues@cern.ch>
2019-05-20 11:58:21 +0200

    Add test suite for DaughtersDict

---
+++ tests/decay/test_decay.py
@@ -0,0 +1,28 @@
+from decaylanguage.decay.decay import DaughtersDict
+
+
+def test_DaughtersDict_constructor():
+    dd = DaughtersDict({'K+': 1, 'K-': 2, 'pi+': 1, 'pi0': 1})
+    assert dd == {'K+': 1, 'K-': 2, 'pi+': 1, 'pi0': 1}
+
+
+def test_DaughtersDict_constructor_from_list():
+    dd = DaughtersDict.from_list(['K+', 'K-', 'K-', 'pi+', 'pi0'])
+    assert dd == {'K+': 1, 'K-': 2, 'pi+': 1, 'pi0': 1}
+
+
+def test_DaughtersDict_string_repr():
+    dd = DaughtersDict.from_list(['K+', 'K-', 'K-', 'pi+', 'pi0'])
+    assert dd.__str__() == "<DaughtersDict: ['K+', 'K-', 'K-', 'pi+', 'pi0']>"
+
+
+def test_DaughtersDict_len():
+    dd = DaughtersDict({'K+': 1, 'K-': 3, 'pi0': 1})
+    assert len(dd) == 5
+
+
+def test_DaughtersDict_add():
+    dd1 = DaughtersDict({'K+': 1, 'K-': 2, 'pi0': 3})
+    dd2 = DaughtersDict({'K+': 1, 'K-': 1})
+    dd3 = dd1 + dd2
+    assert len(dd3) == 8
