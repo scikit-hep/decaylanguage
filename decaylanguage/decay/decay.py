ac801e1c0cc74547dd3a1746eaed5a20c596fb32
eduardo-rodrigues <eduardo.rodrigues@cern.ch>
2019-05-20 11:58:03 +0200

    Make DaughtersDict inherit from collections.Counter

--- decaylanguage/decay/decay.py
+++ decaylanguage/decay/decay.py
@@ -1,5 +1,7 @@
+from collections import Counter
 
-class DaughtersDict(object):
+
+class DaughtersDict(Counter):
     """
     Class holding a decay final state as a dictionary.
     It is a building block for the digital representation of full decay chains.
@@ -12,20 +14,17 @@ class DaughtersDict(object):
 
     __slots__ = ('_daughters')
 
-    def __init__(self, daughters={}):
+    def __init__(self, iterable=None, **kwds):
         """
         Default constructor.
 
         Parameters
         ----------
-        daughters: dict
+        in: dict
             Final state particles represented as a dictionary,
             e.g. ``{'K+': 1, 'K-': 2, 'pi+': 1, 'pi0': 1}``.
         """
-        if not isinstance(daughters, dict):
-            raise RuntimeError('invalid input - dict expected!')
-
-        self._daughters = daughters
+        super(DaughtersDict, self).__init__(iterable, **kwds)
 
     @classmethod
     def from_list(cls, daughters):
@@ -39,13 +38,13 @@ class DaughtersDict(object):
         Return the daughters as a string representation ordered list of names.
         """
         strs = []
-        for d in sorted(self._daughters):
-            strs += [d]*self._daughters[d]
-        return ' '.join(strs)
+        for d in sorted(self):
+            strs += [d]*self[d]
+        return ' '.join(list(self.elements()))
 
     def __repr__(self):
         return "<{self.__class__.__name__}: {daughters}>".format(
-                self=self, daughters=self._daughters)
+                self=self, daughters=sorted(list(self.elements())))
 
     def __str__(self):
         return repr(self)
@@ -58,45 +57,12 @@ class DaughtersDict(object):
         ----
         This is general *not* the number of dictionary elements.
         """
-        return sum(v for v in self._daughters.values())
-
-    def __iadd__(self, other):
-        """
-        Self-addition with another `DaughtersDict` instance, i.e. self += other.
-        """
-        if not isinstance(other, DaughtersDict):
-            raise InvalidOperationError("invalid operation '+=' between a 'DaughtersDict' and a '{0}'".format(other.__class__.__name__))
-
-        self._daughters = {key: self._daughters.get(key, 0) + other._daughters.get(key, 0)
-                           for key in set(self._daughters) | set(other._daughters)}
-        return self
+        return sum(n for n in self.values())
 
     def __add__(self, other):
-        """
-        Addition with another `DaughtersDict` instance.
-        """
-        if not isinstance(other, DaughtersDict):
-            raise InvalidOperationError("invalid operation '+=' between a 'DaughtersDict' and a '{0}'".format(other.__class__.__name__))
+        dd = self.copy()
+        dd += other
+        return dd
 
-        result = DaughtersDict()
-        result._daughters = {key: self._daughters.get(key, 0) + other._daughters.get(key, 0)
-                           for key in set(self._daughters) | set(other._daughters)}
-        return result
-
-    def __eq__(self, other):
-        """
-        Are 2 `DaughtersDict` instances equal?
-
-        Note
-        ----
-        Two instances are equal if, and only if, the same final state particles
-        compose the objects, and in the same number.
-        """
-        # Make sure that each DaughtersDict contains the exact same particles
-        same_particles = set(self._daughters) == (set(self._daughters) | set(other._daughters))
-        if not same_particles:
-            return False
-
-        # Each particle much be in equal number
-        return all(n1==n2 for n1, n2 in
-                   zip(sorted(self._daughters.values()), sorted(other._daughters.values())))
+    def __iter__(self):
+        return self.elements()
