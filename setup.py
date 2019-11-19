9a02b51fe2db7abf001c8a81d690a1f9e6d14857
Eduardo Rodrigues <eduardo.rodrigues@cern.ch>
2019-11-19 10:56:05 +0100

    Add Python 3.8 support, simplify setup.py (#89)

--- setup.py
+++ setup.py
@@ -15,6 +15,23 @@ import os
 from setuptools import find_packages
 from setuptools import setup
 
+
+PYTHON_REQUIRES = ">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*"
+
+INSTALL_REQUIRES = [
+    'attrs>=17.4',
+    'plumbum>=1.6.6',
+    'numpy>=1.12',
+    'pandas>=0.22',
+    'six>=1.11',
+    'lark-parser>=0.6.3',
+    'pathlib2>=2.3; python_version<"3.5"',
+    'enum34>=1.1; python_version<"3.4"',
+    'importlib_resources>=1.0; python_version<"3.7"',
+    'cachetools; python_version<"3.3"',
+    'particle==0.6.*'
+]
+
 needs_pytest = {'pytest', 'test', 'ptr'}.intersection(sys.argv)
 pytest_runner = ['pytest-runner'] if needs_pytest else []
 
@@ -42,20 +59,28 @@ extras = {
 }
 
 setup(
-    name='decaylanguage',
-    author='Henry Fredrick Schreiner III, Eduardo Rodrigues',
-    author_email='henry.schreiner@cern.ch, eduardo.rodrigues@cern.ch',
+    name = 'DecayLanguage',
+    author = 'Henry Fredrick Schreiner III, Eduardo Rodrigues',
+    author_email = 'henry.schreiner@cern.ch, eduardo.rodrigues@cern.ch',
     maintainer = 'The Scikit-HEP admins',
     maintainer_email = 'scikit-hep-admins@googlegroups.com',
     version = get_version(),
-    license='BSD 3-Clause License',
-    description='A language to describe particle decays, and tools to work with them.',
-    long_description=proc_readme(read('README.md')) + '\n\n' + read('CHANGELOG.md'),
-    long_description_content_type="text/markdown",
-    url='https://github.com/scikit-hep/decaylanguage',
-    packages=find_packages(),
-    package_data={'': ['data/*.*']},
-    classifiers=[
+    license = 'BSD 3-Clause License',
+    description = 'A language to describe particle decays, and tools to work with them.',
+    long_description = proc_readme(read('README.md')) + '\n\n' + read('CHANGELOG.md'),
+    long_description_content_type = "text/markdown",
+    url = 'https://github.com/scikit-hep/decaylanguage',
+    packages = find_packages(),
+    package_data = {'': ['data/*.*']},
+    python_requires = PYTHON_REQUIRES,
+    install_requires = INSTALL_REQUIRES,
+    setup_requires = [] + pytest_runner,
+    tests_require = extras['test'],
+    extras_require = extras,
+    keywords = [
+        'HEP', 'particle', 'decay', 'representation'
+    ],
+    classifiers = [
         # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
@@ -68,26 +93,8 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Topic :: Scientific/Engineering',
     ],
-    keywords=[
-        'particle', 'decay', 'HEP'
-    ],
-    install_requires=[
-        'attrs>=17.4',
-        'plumbum>=1.6.6',
-        'numpy>=1.12',
-        'pandas>=0.22',
-        'six>=1.11',
-        'lark-parser>=0.6.3',
-        'pathlib2>=2.3; python_version<"3.5"',
-        'enum34>=1.1; python_version<"3.4"',
-        'importlib_resources>=1.0; python_version<"3.7"',
-        'cachetools; python_version<"3.3"',
-        'particle==0.6.*'
-    ],
-    extras_require=extras,
-    setup_requires=[] + pytest_runner,
-    tests_require=extras['test'],
-    platforms="Any",
+    platforms = "Any",
 )
