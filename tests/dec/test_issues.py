36175aa8bd50237f9b161690942580d5e5fc4d71
eduardo-rodrigues <eduardo.rodrigues@cern.ch>
2020-01-06 15:54:43 +0100

    Add test for issue #90

---
+++ tests/dec/test_issues.py
@@ -0,0 +1,23 @@
+# Copyright (c) 2020, Eduardo Rodrigues and Henry Schreiner.
+#
+# Distributed under the 3-clause BSD license, see accompanying file LICENSE
+# or https://github.com/scikit-hep/decaylanguage for details.
+
+import pytest
+from lark import UnexpectedToken
+
+try:
+    from pathlib2 import Path
+except ImportError:
+    from pathlib import Path
+
+from decaylanguage.dec.dec import DecFileParser
+
+
+DIR = Path(__file__).parent.resolve()
+
+
+def test_issue_90():
+    with pytest.raises(UnexpectedToken):
+        p = DecFileParser(DIR / '../data/test_issue90.dec')
+        p.parse()
