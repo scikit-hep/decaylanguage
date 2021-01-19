26b9ed9b90a04915bb56cd6c6ebd771c1c0d1c33
Eduardo Rodrigues <eduardo.rodrigues@cern.ch>
2021-01-19 19:52:57 +0100

    Bump version to 0.10.2 (#122)
    * Bump version to 0.10.2
    
    * Update copyright statements to 2021

--- decaylanguage/_version.py
+++ decaylanguage/_version.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
-# Copyright (c) 2018-2020, Eduardo Rodrigues and Henry Schreiner.
+# Copyright (c) 2018-2021, Eduardo Rodrigues and Henry Schreiner.
 #
 # Distributed under the 3-clause BSD license, see accompanying file LICENSE
 # or https://github.com/scikit-hep/decaylanguage for details.
 
 
-__version__ = "0.10.1"
+__version__ = "0.10.2"
 
 version = __version__
 version_info = __version__.split(".")
