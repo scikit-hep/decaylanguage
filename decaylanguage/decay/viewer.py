e9d8a0bde68249f92bd3a7bf846d9ad9286af5ca
Eduardo Rodrigues <eduardo.rodrigues@cern.ch>
2021-03-24 20:18:22 +0100

    Make DecayChainViewer use graphviz rather then pydot, which is not maintained

--- decaylanguage/decay/viewer.py
+++ decaylanguage/decay/viewer.py
@@ -17,12 +17,7 @@ try:
 except AttributeError:
     counter = itertools.count().next
 
-try:
-    import pydot
-except ImportError:
-    raise ImportError(
-        "You need pydot for this submodule. Please install pydot with for example 'pip install pydot'\n"
-    )
+import graphviz
 
 from particle import latex_to_html_name
 from particle.converters.bimap import DirectionalMaps
@@ -39,13 +34,17 @@ class DecayChainViewer(object):
     """
     The class to visualize a decay chain.
 
-    Example
-    -------
+    Examples
+    --------
     >>> dfp = DecFileParser('my-Dst-decay-file.dec')
     >>> dfp.parse()
     >>> chain = dfp.build_decay_chains('D*+')
     >>> dcv = DecayChainViewer(chain)
     >>> dcv  # display the SVG figure in a notebook
+
+    When not in notebooks the graph can easily be visualized with the
+    `graphviz.dot.Digraph.render` or `graphviz.dot.Digraph.view` functions, e.g.:
+    >>> dcv.graph.render(filename="test", format="pdf", view=True, cleanup=True)
     """
 
     __slots__ = ("_chain", "_graph", "_graph_attributes")
@@ -59,7 +58,7 @@ class DecayChainViewer(object):
         decaychain: dict
             Input .dec decay file name.
         attrs: optional
-            User input pydot.Dot class attributes.
+            User input `graphviz.dot.Digraph` class attributes.
 
         See also
         --------
@@ -68,7 +67,7 @@ class DecayChainViewer(object):
         # Store the input decay chain
         self._chain = decaychain
 
-        # Instantiate digraph with defaults possibly overriden by user attributes
+        # Instantiate the digraph with defaults possibly overriden by user attributes
         self._graph = self._instantiate_graph(**attrs)
 
         # Build the actual graph from the input decay chain structure
@@ -76,7 +75,7 @@ class DecayChainViewer(object):
 
     def _build_decay_graph(self):
         """
-        Recursively navigate the decay chain tree and produce a Graph
+        Recursively navigate the decay chain tree and produce a Digraph
         in the DOT language.
         """
 
@@ -124,9 +123,7 @@ class DecayChainViewer(object):
         def new_node_no_subchain(list_parts):
             label = html_table_label(list_parts, bgcolor="#eef3f8")
             r = "dec%s" % counter()
-            self._graph.add_node(
-                pydot.Node(r, label=label, style="filled", fillcolor="#eef3f8")
-            )
+            self.graph.node(r, label=label, style="filled", fillcolor="#eef3f8")
             return r
 
         def new_node_with_subchain(list_parts):
@@ -135,12 +132,13 @@ class DecayChainViewer(object):
             ]
             label = html_table_label(list_parts, add_tags=True)
             r = "dec%s" % counter()
-            self._graph.add_node(pydot.Node(r, shape="none", label=label))
+            self.graph.node(r, shape="none", label=label)
             return r
 
         def iterate_chain(subchain, top_node=None, link_pos=None):
             if not top_node:
-                top_node = node_mother
+                top_node = "mother"
+                self.graph.node("mother", shape="none", label=label)
             n_decaymodes = len(subchain)
             for idm in range(n_decaymodes):
                 _list_parts = subchain[idm]["fs"]
@@ -148,28 +146,22 @@ class DecayChainViewer(object):
                     _ref = new_node_no_subchain(_list_parts)
                     _bf = subchain[idm]["bf"]
                     if link_pos is None:
-                        self._graph.add_edge(pydot.Edge(top_node, _ref, label=str(_bf)))
+                        self.graph.edge(top_node, _ref, label=str(_bf))
                     else:
-                        self._graph.add_edge(
-                            pydot.Edge(
+                        self.graph.edge(
                                 "%s:p%s" % (top_node, link_pos), _ref, label=str(_bf)
                             )
-                        )
                 else:
                     _ref_1 = new_node_with_subchain(_list_parts)
                     _bf_1 = subchain[idm]["bf"]
                     if link_pos is None:
-                        self._graph.add_edge(
-                            pydot.Edge(top_node, _ref_1, label=str(_bf_1))
-                        )
+                        self.graph.edge(top_node, _ref_1, label=str(_bf_1))
                     else:
-                        self._graph.add_edge(
-                            pydot.Edge(
+                        self.graph.edge(
                                 "%s:p%s" % (top_node, link_pos),
                                 _ref_1,
                                 label=str(_bf_1),
                             )
-                        )
                     for i, _p in enumerate(_list_parts):
                         if not isinstance(_p, str):
                             _k = list(_p.keys())[0]
@@ -179,80 +171,79 @@ class DecayChainViewer(object):
 
         k = list(self._chain.keys())[0]
         label = html_table_label([k], add_tags=True, bgcolor="#568dba")
-        node_mother = pydot.Node("mother", shape="none", label=label)
-        self._graph.add_node(node_mother)
         sc = self._chain[k]
 
         # Actually build the whole decay chain, iteratively
         iterate_chain(sc)
 
-    def visualize_decay_graph(self, format="png"):
-        """
-        Visualize the Graph produced, opening the file ('png' by default)
-        with the machine default program.
-        """
-        import tempfile
-        import webbrowser
-
-        tmpf = tempfile.NamedTemporaryFile(
-            prefix="DecayChainViewer", suffix=".{0}".format(format), delete=False
-        )
-        self.graph.write(tmpf.name, format=format)
-        tmpf.close()
-        return webbrowser.open(tmpf.name)
-
     @property
     def graph(self):
         """
-        Get the actual Graph. The user now has full control ...
+        Get the actual `graphviz.dot.Digraph` object.
+        The user now has full control ...
         """
         return self._graph
 
     def to_string(self):
         """
-        Return a string representation of the built Graph in the DOT language.
-        The function is a trivial shortcut for `Graph.to_string()`.
+        Return a string representation of the built graph in the DOT language.
+        The function is a trivial shortcut for ``graphviz.dot.Digraph.source`.
         """
-        return self.graph.to_string()
+        return self.graph.source
 
     def _instantiate_graph(self, **attrs):
         """
-        Return a pydot.Dot class instance using the default attributes
+        Return a ``graphviz.dot.Digraph` class instance using the default attributes
         specified in this class:
         - Default graph attributes are overriden by input by the user.
         - Class and node and edge defaults.
         """
-        # Make sure the user cannot override the graph type
-        try:
-            del attrs["graph_type"]
-        except KeyError:
-            pass
+        graph_attr = self._get_graph_defaults()
+        node_attr = self._get_node_defaults()
+        edge_attr = self._get_edge_defaults()
+        if "graph_attr" in attrs:
+            graph_attr.update(**attrs["graph_attr"])
+            attrs.pop("graph_attr")
+        if "node_attr" in attrs:
+            node_attr.update(**attrs["node_attr"])
+            attrs.pop("node_attr")
+        if "edge_attr" in attrs:
+            edge_attr.update(**attrs["edge_attr"])
+            attrs.pop("edge_attr")
 
-        default_args = self._get_graph_defaults()
-        default_args.update(**attrs)
+        arguments = self._get_default_arguments()
+        arguments.update(**attrs)
 
-        g = pydot.Dot(**default_args)
-
-        g.set_node_defaults(**self._get_node_defaults())
-        g.set_edge_defaults(**self._get_edge_defaults())
+        g = graphviz.Digraph(**arguments,
+                             graph_attr=graph_attr,
+                             node_attr=node_attr,
+                             edge_attr=edge_attr
+                             )
 
         return g
 
+    def _get_default_arguments(self):
+        """
+        `graphviz.dot.Digraph` default arguments.
+        """
+        return dict(name="DecayChainGraph",
+                    comment="Created by https://github.com/scikit-hep/decaylanguage",
+                    engine="dot",
+                    format="png")
+
     def _get_graph_defaults(self):
-        """
-        Note: the graph type cannot be overriden.
-        """
-        return dict(graph_type="digraph", graph_name="DecayChainGraph", rankdir="LR")
+        d = self._get_default_arguments()
+        d.update(rankdir="LR")
+        return d
 
     def _get_node_defaults(self):
-        return dict(fontname="Helvetica", fontsize=11, shape="oval")
+        return dict(fontname="Helvetica", fontsize="11", shape="oval")
 
     def _get_edge_defaults(self):
-        return dict(fontcolor="#4c4c4c", fontsize=11)
+        return dict(fontcolor="#4c4c4c", fontsize="11")
 
     def _repr_svg_(self):
         """
         IPython display in SVG format.
         """
-
-        return self.graph.create_svg().decode("UTF-8")
+        return self.graph
