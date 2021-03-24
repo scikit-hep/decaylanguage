43772edbefe3167129f2dc94c41c9e0d0028427a
Eduardo Rodrigues <eduardo.rodrigues@cern.ch>
2021-03-24 22:11:57 +0100

    Fix missing comma
    Co-authored-by: Henry Schreiner <henry.fredrick.schreiner@cern.ch>

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ setup(
     install_requires=INSTALL_REQUIRES,
     tests_require=extras["test"],
     extras_require=extras,
-    keywords=["HEP", "particle", "decay", "chain" "representation"],
+    keywords=["HEP", "particle", "decay", "chain", "representation"],
     classifiers=[
         # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
         "Development Status :: 4 - Beta",
