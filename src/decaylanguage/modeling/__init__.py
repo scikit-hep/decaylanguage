ee01c320c0ab405c859beab1064aeb51bd8e798c
Eduardo Rodrigues <eduardo.rodrigues@cern.ch>
2022-01-17 18:16:15 +0100

    Get started with __init__.py files

--- src/decaylanguage/modeling/__init__.py
+++ src/decaylanguage/modeling/__init__.py
@@ -1,7 +1,14 @@
+# Copyright (c) 2018-2022, Eduardo Rodrigues and Henry Schreiner.
+#
+# Distributed under the 3-clause BSD license, see accompanying file LICENSE
+# or https://github.com/scikit-hep/decaylanguage for details.
+
+from typing import Tuple
+
 from .amplitudechain import LS, AmplitudeChain
 
 __all__ = ("LS", "AmplitudeChain")
 
 
-def __dir__():
+def __dir__() -> Tuple[str, ...]:
     return __all__
