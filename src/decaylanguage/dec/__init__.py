263343fa2b94511f5fe60e63e115e278b57efe6d
Eduardo Rodrigues <eduardo.rodrigues@cern.ch>
2022-01-18 09:43:40 +0100

    Added checking to utils/utilities.py

--- src/decaylanguage/dec/__init__.py
+++ src/decaylanguage/dec/__init__.py
@@ -1,8 +1,14 @@
+# Copyright (c) 2018-2022, Eduardo Rodrigues and Henry Schreiner.
+#
+# Distributed under the 3-clause BSD license, see accompanying file LICENSE
+# or https://github.com/scikit-hep/decaylanguage for details.
+
+from typing import Tuple
 from .dec import DecFileParser
 from .enums import known_decay_models
 
 __all__ = ("DecFileParser", "known_decay_models")
 
 
-def __dir__():
+def __dir__() -> Tuple[str, ...]:
     return __all__
