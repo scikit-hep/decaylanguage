218ab7d214801b4069f58b96b4405a162ba00bda
Eduardo Rodrigues <eduardo.rodrigues@cern.ch>
2022-01-20 11:13:51 +0100

    Improve type ignores in decay/viewer.py

--- src/decaylanguage/decay/viewer.py
+++ src/decaylanguage/decay/viewer.py
@@ -10,9 +10,10 @@ see the `DecFileParser` class.
 """
 
 import itertools
+import graphviz
+
 from typing import Any, Optional, Union
 
-import graphviz
 from particle import latex_to_html_name
 from particle.converters.bimap import DirectionalMaps
 
@@ -152,15 +153,15 @@ class DecayChainViewer:
             n_decaymodes = len(subchain)
             for idm in range(n_decaymodes):
                 _list_parts = subchain[idm]["fs"]
-                if not has_subdecay(_list_parts):  # type: ignore
-                    _ref = new_node_no_subchain(_list_parts)  # type: ignore
+                if not has_subdecay(_list_parts):  # type: ignore [arg-type]
+                    _ref = new_node_no_subchain(_list_parts)  # type: ignore [arg-type]
                     _bf = subchain[idm]["bf"]
                     if link_pos is None:
                         self.graph.edge(top_node, _ref, label=str(_bf))
                     else:
                         self.graph.edge(f"{top_node}:p{link_pos}", _ref, label=str(_bf))
                 else:
-                    _ref_1 = new_node_with_subchain(_list_parts)  # type: ignore
+                    _ref_1 = new_node_with_subchain(_list_parts)  # type: ignore [arg-type]
                     _bf_1 = subchain[idm]["bf"]
                     if link_pos is None:
                         self.graph.edge(top_node, _ref_1, label=str(_bf_1))
@@ -170,7 +171,7 @@ class DecayChainViewer:
                             _ref_1,
                             label=str(_bf_1),
                         )
-                    for i, _p in enumerate(_list_parts):  # type: ignore
+                    for i, _p in enumerate(_list_parts):  # type: ignore [arg-type]
                         if not isinstance(_p, str):
                             _k = list(_p.keys())[0]
                             iterate_chain(_p[_k], top_node=_ref_1, link_pos=i)
@@ -198,7 +199,7 @@ class DecayChainViewer:
         Return a string representation of the built graph in the DOT language.
         The function is a trivial shortcut for ``graphviz.dot.Digraph.source`.
         """
-        return self.graph.source  # type: ignore
+        return self.graph.source  # type: ignore [no-any-return]
 
     def _instantiate_graph(
         self, **attrs: dict[str, Union[bool, int, float, str]]
@@ -223,7 +224,7 @@ class DecayChainViewer:
             attrs.pop("edge_attr")
 
         arguments = self._get_default_arguments()
-        arguments.update(**attrs)  # type: ignore
+        arguments.update(**attrs)  # type: ignore [call-overload]
 
         return graphviz.Digraph(
             graph_attr=graph_attr, node_attr=node_attr, edge_attr=edge_attr, **arguments
