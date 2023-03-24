8ecfdf696e75bd724c3cd219f201883aae0d2963
Eduardo Rodrigues <eduardo.rodrigues@cern.ch>
2023-03-24 13:10:57 +0100

    Move particle finding functions for AmpGen names to this package (#312)
    * Copy AmpGen relevant particle finding functions to this package
    
    * Move related tests over as well
    
    * style: pre-commit fixes
    
    * Forgot to add a little helper
    
    * style: pre-commit fixes
    
    * Fixes for MyPy and missing import
    
    * style: pre-commit fixes
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- tests/utils/test_particleutils.py
+++ tests/utils/test_particleutils.py
@@ -6,8 +6,12 @@
 from __future__ import annotations
 
 import pytest
+from particle import ParticleNotFound
 
-from decaylanguage.utils.particleutils import charge_conjugate_name
+from decaylanguage.utils.particleutils import (
+    charge_conjugate_name,
+    particle_from_string_name,
+)
 
 matches_evtgen = (
     ("pi0", "pi0"),
@@ -34,3 +38,44 @@ def test_charge_conjugate_name_defaults(p_name, ccp_name):
 @pytest.mark.parametrize(("p_name", "ccp_name"), matches_pdg)
 def test_charge_conjugate_name_with_pdg_name(p_name, ccp_name):
     assert charge_conjugate_name(p_name, pdg_name=True) == ccp_name
+
+
+def test_particle_from_string_name():
+    pi = particle_from_string_name("pi+")
+    assert pi.pdgid == 211
+
+    with pytest.raises(ParticleNotFound):
+        particle_from_string_name("unknown")
+
+
+def test_fuzzy_string():
+    """
+    The input name is not specific enough, in which case the search is done
+    by pdg_name after failing a match by name.
+    """
+    p = particle_from_string_name("a(0)(980)")  # all 3 charge stages match
+    assert p.pdgid == 9000111
+
+
+ampgen_style_names = (
+    ("b", 5),
+    ("b~", -5),
+    ("pi+", 211),
+    ("pi-", -211),
+    ("K~*0", -313),
+    ("K*(892)bar0", -313),
+    ("a(1)(1260)+", 20213),
+    ("rho(1450)0", 100113),
+    ("rho(770)0", 113),
+    ("K(1)(1270)bar-", -10323),
+    # ("K(1460)bar-", -100321),
+    ("K(2)*(1430)bar-", -325),
+)
+
+
+@pytest.mark.parametrize(("name", "pid"), ampgen_style_names)
+def test_ampgen_style_names(name, pid):
+    particle = particle_from_string_name(name)
+
+    assert particle.pdgid == pid
+    assert particle == pid
