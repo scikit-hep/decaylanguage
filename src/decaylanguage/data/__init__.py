defa1daaa1c4e70667417564c4dcad8eb6066e44
Eduardo Rodrigues <eduardo.rodrigues@cern.ch>
2023-07-11 10:21:58 +0200

    [chore] Remove deprecated function in decaylanguage.data (#346)
    * Remove deprecated function
    
    * Remove unnecessary imports
    
    * style: pre-commit fixes
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- src/decaylanguage/data/__init__.py
+++ src/decaylanguage/data/__init__.py
@@ -2,20 +2,13 @@ from __future__ import annotations
 
 import sys
 
-from deprecated import deprecated
-
 if sys.version_info < (3, 9):
     import importlib_resources as resources
 else:
     from importlib import resources
 
 
-__all__ = ["basepath", "open_text"]
+__all__ = ["basepath"]
 
 
 basepath = resources.files(__name__)
-
-
-open_text = deprecated(
-    version="0.12.0", reason="Use decaylanguage.data.basepath instead."
-)(resources.open_text)
