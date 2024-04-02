cf35b88b79a0e76677ba56b77c7b546521fc9906
Henry Schreiner <henry.fredrick.schreiner@cern.ch>
2024-04-02 07:35:43 -0400

    chore: drop Python 3.7, add Python 3.12 (#428)
    * chore: drop Python 3.7, add Python 3.12
    
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>
    
    * Fix little typo
    
    ---------
    
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>
    Co-authored-by: Eduardo Rodrigues <eduardo.rodrigues@cern.ch>

--- src/decaylanguage/_compat/typing.py
+++ src/decaylanguage/_compat/typing.py
@@ -3,11 +3,6 @@ from __future__ import annotations
 import sys
 import typing
 
-if sys.version_info < (3, 8):
-    from typing_extensions import TypedDict
-else:
-    from typing import TypedDict
-
 if sys.version_info < (3, 11):
     if typing.TYPE_CHECKING:
         from typing_extensions import Self
@@ -17,4 +12,4 @@ else:
     from typing import Self
 
 
-__all__ = ["TypedDict", "Self"]
+__all__ = ["Self"]
