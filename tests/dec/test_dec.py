51d04de2d482e57d9f43ae365c36b46e02b02137
Eduardo Rodrigues <eduardo.rodrigues@cern.ch>
2024-08-09 17:21:36 +0200

    EvtGen uses GeV, not the standard HEP system of units, hence MeV! (#455)
    * Add hepunits as an explicit dependency
    
    * Adapt to the fact that EvtGen uses GeV rather than MeV!
    
    * Adapt tests accordingly, and enhance them
    
    * style: pre-commit fixes
    
    * Fix a silly typo!
    
    * Please MyPy since the try statement accommodates the optional return
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- tests/dec/test_dec.py
+++ tests/dec/test_dec.py
@@ -197,7 +197,8 @@ def test_particle_property_definitions():
         "MyK*0": {"mass": 0.892, "width": 0.051},
         "MyPhi": {"mass": 1.02, "width": 0.004},
         "rho0": {"mass": 0.8, "width": 0.2},
-        "MyRho0": {"mass": 0.8, "width": 147.4},
+        "MyRho0": {"mass": 0.77, "width": 0.1474},
+        "chi_c0": {"mass": 3.42, "width": 0.0107},
     }
 
 
