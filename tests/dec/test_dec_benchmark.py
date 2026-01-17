8b31cdc526e61befdfe6434f9c66e046118fd4b6
Vidya Sagar VOBBILISETTI <vidya.vobbilisetti@pi.infn.it>
2026-01-17 01:12:05 +0100

    Add benchmark tests for minimum_effective_bf

--- tests/dec/test_dec_benchmark.py
+++ tests/dec/test_dec_benchmark.py
@@ -33,4 +33,17 @@ def test_build_decay_chains_benchmark(belle2_dec_file, benchmark):
         build_decay_chains_benchmark, args=(belle2_dec_file,), rounds=3
     )
     assert result
+
+
+def build_decay_chains_minimum_effective_bf_benchmark(p):
+    return p.build_decay_chains("D_s*+", minimum_effective_bf=0.1)
+
+
+@pytest.mark.benchmark
+def test_build_decay_chains_min_bf_benchmark(belle2_dec_file, benchmark):
+    result = benchmark.pedantic(
+        build_decay_chains_minimum_effective_bf_benchmark,
+        args=(belle2_dec_file,),
+        rounds=3,
+    )
     assert result
